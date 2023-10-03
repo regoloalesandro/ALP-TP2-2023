d82a4def134a2686e12d14b3b3d3a55162246508
regoloalesandro <41971839+regoloalesandro@users.noreply.github.com>
2023-10-03 17:22:31 -0300

    #1 - ejercicios 1 y 2 andando

---
+++ TP2/src/Untyped.hs
@@ -0,0 +1,49 @@
+module Untyped where
+
+import           Control.Monad
+import           Data.List
+import           Data.Maybe
+
+import           Common
+
+----------------------------------------------
+-- Seccón 2  
+-- Ejercicio 2: Conversión a términos localmente sin nombres
+----------------------------------------------
+
+conversion :: LamTerm -> Term
+conversion = conversion' []
+conversion' :: [String] -> LamTerm -> Term
+conversion vars (LVar xs) = case (elemIndex xs vars) of 
+                               Nothing -> Free (Global xs)
+                               Just n  -> (Bound n)
+conversion vars (App lt1 lt2)  = conversion' vars lt1  :@: conversion' vars lt2  
+conversion vars (Abs var lt)  = Lam conversion' (var:vars) lt 
+-------------------------------
+-------------------------------
+-- Sección 3
+-------------------------------
+
+vapp :: Value -> Value -> Value
+vapp = undefined
+
+eval :: NameEnv Value -> Term -> Value
+eval e t = eval' t (e, [])
+
+eval' :: Term -> (NameEnv Value, [Value]) -> Value
+eval' (Bound ii) (_, lEnv) = lEnv !! ii
+eval' _          _         = undefined
+
+
+--------------------------------
+-- Sección 4 - Mostrando Valores
+--------------------------------
+
+quote :: Value -> Term
+quote = undefined
+
+
+
+
+
+
